cbab016e2caaaa0fc109969236272490c86f280c
John <mur.sensi@yandex.ru>
2022-08-10 19:19:39 +0300

    HOC
    HOC add

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+// high order component - пример, с передачей пропсов
+
 import Item from '../Item';
+import withClass from '../withClass/withClass';
 import 'antd/dist/antd.min.css';
 import './App.scss';
 
 function App() {
-  return <Item />;
+  const NewTest = withClass(Item, '2222'); // HOC
+  return (
+    <>
+      <Item />
+      <Item name='Anya' />
+      <NewTest name='Anyta' /> {/* HOC */}
+    </>
+  );
 }
-export default App;
+
+export default withClass(App, 'test-name-class-super');
