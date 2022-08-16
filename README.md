ba14d7952550c35b7cef9d52058f797e6e39a5cd
Sensibleness <you@example.com>
2022-08-16 13:42:27 +0300

    a
    a

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-https://moviedb-7nvdkixut-tosha-666.vercel.app/
+React Learn
---
+++ src/prop-types-info/prop-types-info.js
@@ -0,0 +1,5 @@
+// ▬▬▬▬▬▬▬
+// Основная ссылка
+// https://ru.react.js.org/docs/typechecking-with-proptypes.html
+
+// ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
